faa5c7bd0db6bbb07b6ebb6e4a36368bb5d97de7
Nathaniel Schaaf <schaafna@gmail.com>
2020-05-08 12:40:15 +0200

    Add config loading / saving test

--- SchanaModPartyTest/Mission/schana_party_manager_client.c
+++ SchanaModPartyTest/Mission/schana_party_manager_client.c
@@ -2,11 +2,13 @@ modded class SchanaPartyManagerClient {
     void SchanaPartyManagerClient () {
         SchanaPartyUtils.LogMessage ("Test Client Init " + MissionBase.SCHANA_PARTY_VERSION);
 
-        GetGame ().GetCallQueue (CALL_CATEGORY_GUI).CallLater (this.MockUpdate, 5000, true);
+        // GetGame ().GetCallQueue (CALL_CATEGORY_GUI).CallLater (this.MockUpdate, 5000, true);
+        GetGame ().GetCallQueue (CALL_CATEGORY_GUI).CallLater (this.TestConfigLoading, 200, true);
     }
 
     void ~SchanaPartyManagerClient () {
-        GetGame ().GetCallQueue (CALL_CATEGORY_GUI).Remove (this.MockUpdate);
+        // GetGame ().GetCallQueue (CALL_CATEGORY_GUI).Remove (this.MockUpdate);
+        GetGame ().GetCallQueue (CALL_CATEGORY_GUI).Remove (this.TestConfigLoading);
     }
 
     void MockUpdate () {
@@ -20,5 +22,16 @@ modded class SchanaPartyManagerClient {
             auto data = new Param2<ref string, ref array<ref string>> (i.ToString (), members);
             GetRPCManager ().SendRPC ("SchanaModParty", "ServerRegisterPartyRPC", data);
         }
+    }
+
+    int test_config_counter = 0;
+
+    void TestConfigLoading () {
+        auto test_settings = SchanaModPartySettings.Get ();
+        if (test_config_counter > 0 && !test_settings.Contains ((test_config_counter - 1).ToString ())) {
+            Print("[SchanaParty] Test Error loading configuration");
+        }
+        test_settings.Add (test_config_counter.ToString (), "name " + test_config_counter.ToString ());
+        test_config_counter++;
     }
 }
